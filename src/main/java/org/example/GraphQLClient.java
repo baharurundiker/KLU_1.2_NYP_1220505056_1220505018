69996cd2a28afa67bb10446a29019e2ee3c71982
Bahar Üründiker <77542846+baharurundiker@users.noreply.github.com>
2023-12-10 00:50:57 +0300

    Update GraphQLClient.java

--- src/main/java/org/example/GraphQLClient.java
+++ src/main/java/org/example/GraphQLClient.java
@@ -7,40 +7,48 @@ import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class GraphQLClient {
+    /*
+    https://www.google.com/search endpointine hello query si göndermek istedim fakat galiba google graphql sunucusu değil.
+    Bu yüzden loglarda 405 response code unu görüyorum.
+    Method Not Allowed
+     */
+    private static final String GRAPHQL_ENDPOINT = "https://www.google.com/search";
 
-    private final GraphQL graphQL;
+    public static void main(String[] args) {
+        String sampleQuery = "{ \"query\": \"{ hello }\" }";
+        executeGraphQLQuery(sampleQuery);
+    }
 
-    public GraphQLClient() {
-        // GraphQL şemasını tanımlayın
-        String graphqlSchema = "type Query { hello: String }";
-        SchemaParser schemaParser = new SchemaParser();
-        TypeDefinitionRegistry typeRegistry = schemaParser.parse(graphqlSchema);
+    public static void executeGraphQLQuery(String query) {
+        OkHttpClient client = new OkHttpClient();
 
-        // Runtime wiring ayarlarını yapın
-        RuntimeWiring wiring = RuntimeWiring.newRuntimeWiring()
-                .type("Query", builder -> builder.dataFetcher("hello", environment -> "world"))
+        // GraphQL isteği oluştur
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), "{\"query\":\"" + query + "\"}");
+
+        Request request = new Request.Builder()
+                .url(GRAPHQL_ENDPOINT)
+                .addHeader("Content-Type", "application/json")
+                .post(requestBody)
                 .build();
 
-        // GraphQL şemasını oluşturun
-        SchemaGenerator schemaGenerator = new SchemaGenerator();
-        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, wiring);
-
-        // GraphQL nesnesini oluşturun
-        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
-    }
-
-    public Object executeQuery(String query) {
-        // GraphQL sorgusunu yürütmek için yazıldı
-        ExecutionResult result = graphQL.execute(query);
-
-      if (result.getErrors() != null && !result.getErrors().isEmpty()) {
-              // Hataları yazdır veya logla
-            System.out.println("GraphQL sorgu hatası: " + result.getErrors());
+        try (Response response = client.newCall(request).execute()) {
+            // Response'ı yazdır
+            if (response.isSuccessful()) {
+                System.out.println("Response: " + Objects.requireNonNull(response.body()).string());
+            } else {
+                System.out.println("Error: " + response.code() + " - " + response.message());
+            }
+            //hataları yazdır.
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("An unexpected error occurred: " + e.getMessage());
         }
-
-
-        return result.getData();
     }
-
 }
